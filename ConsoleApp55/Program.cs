898736278bfcc2843a65d2616007c4d531e3d862
sulton <global-sultonmalik326@gmail.com>
2024-01-17 22:13:11 +0500

    Add project files.

---
+++ ConsoleApp55/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Telegram.Bot;
+
+
+namespace ConsoleApp55;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+
+        const string token = "6963507776:AAGytlWgOC5Y4WYBRp7utDo1StTAlwm8vvc";
+
+        TelegramPost telegramPost = new TelegramPost(token);
+        await telegramPost.PostHandle();
+      
+
+    }
+}
---
+++ ConsoleApp55/TelegramPost.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace ConsoleApp55
+{
+    public class TelegramPost
+    {
+        private string Token { get; set; }
+        private string ChannelName { get; set; }
+        private string PostText { get; set; }
+
+        public TelegramPost(string token)
+        {
+            this.Token = token;
+        }
+
+        public async Task PostHandle()
+        {
+            var botClient = new TelegramBotClient(this.Token);
+
+            using CancellationTokenSource cts = new();
+
+            ReceiverOptions receiverOptions = new()
+            {
+                AllowedUpdates = Array.Empty<UpdateType>()
+            };
+
+            botClient.StartReceiving(
+                updateHandler:  HandleUpdateAsync,
+                pollingErrorHandler:  HandlePollingErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: cts.Token
+            );
+
+            var me = await botClient.GetMeAsync();
+
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+
+            cts.Cancel();
+        }
+
+        private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Message is not { } message || message.Text is not { } messageText)
+                return;
+
+            var chatId = message.Chat.Id;
+
+            if (messageText.StartsWith("/createpost"))
+            {
+                ChannelName = null; 
+                PostText = null;   
+
+                await botClient.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: $"Enter channel name: (After entering, send '#')",
+                    cancellationToken: cancellationToken);
+            }
+            else if (ChannelName == null)
+            {
+                ChannelName = messageText.Trim('#');
+
+                await botClient.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: $"Enter post text:",
+                    cancellationToken: cancellationToken);
+            }
+            else if (PostText == null)
+            {
+                PostText = messageText;
+
+                
+                await botClient.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: $"Channel: {ChannelName}\nPost: {PostText}\n\nYou can now perform actions like 'Postni Joylash', 'Postni Edit Qilish', etc.",
+                    cancellationToken: cancellationToken);
+            }
+        }
+
+        private async Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var errorMessage = exception switch
+            {
+                ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(errorMessage);
+            await Task.CompletedTask;
+        }
+    }
+}
